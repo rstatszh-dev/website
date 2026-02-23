b10fe7be9db9ca1dcb7b4b68826281f36dd2f5ee
larnsce <lars@lse.de>
2026-02-23 19:36:37 +0200

    daten aktualisieren

--- data/importiere_kursdaten.R
+++ data/importiere_kursdaten.R
@@ -31,7 +31,7 @@ read_sheet(ss = kursplan) |>
 projekte <- "https://docs.google.com/spreadsheets/d/1jWY1q1H14Zhb-c4BXgRp3K9QqdfHCX6nrkEZjQ5TQX4/edit?gid=1700884386#gid=1700884386"
 
 read_sheet(ss = projekte) |> 
-    filter(kurs == "k010") |> 
+    filter(kurs %in% c("k010", "k011")) |> 
     select(gh_username = `Bitte gib deinen GitHub-Benutzernamen an`,
            kurs,
            direktion = `In welcher Direktion des Kanton Zürich bist du angestellt?`,
